ca7ba31a43d38f2de8cfeb3dd56958af9055a78e
Witold Szrama <witold.szrama@programa.pl>
2020-04-27 13:55:45 +0200

    Add translations

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,57 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MainController extends AbstractController
 {
     /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
      * @Route("/", name="main")
      */
-    public function index()
+    public function index(Request $request)
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/pl", name="pl")
+     */
+    public function pl(Request $request)
+    {
+        $this->setLocale($request,'pl');
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/en", name="en")
+     */
+    public function en(Request $request)
+    {
+        $this->setLocale($request,'en');
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+
+    private function setLocale(Request $request,string $locale): void
+    {
+        $request->setLocale($locale);
+        $request->getSession()->set('_locale', $locale);
+    }
+
 }
