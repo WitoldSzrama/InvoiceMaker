628e74f4eb3626efa76462cc6cc72e81bd173f3e
Witold Szrama <witold.szrama@programa.pl>
2020-04-28 10:20:54 +0200

    Add company entity

---
+++ src/Entity/Company.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
+ */
+class Company
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nip;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $regon;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $postCode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stNumber;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $accountNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNip(): ?int
+    {
+        return $this->nip;
+    }
+
+    public function setNip(?int $nip): self
+    {
+        $this->nip = $nip;
+
+        return $this;
+    }
+
+    public function getRegon(): ?int
+    {
+        return $this->regon;
+    }
+
+    public function setRegon(?int $regon): self
+    {
+        $this->regon = $regon;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPostCode(): ?string
+    {
+        return $this->postCode;
+    }
+
+    public function setPostCode(string $postCode): self
+    {
+        $this->postCode = $postCode;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getStNumber(): ?int
+    {
+        return $this->stNumber;
+    }
+
+    public function setStNumber(int $stNumber): self
+    {
+        $this->stNumber = $stNumber;
+
+        return $this;
+    }
+
+    public function getAccountNumber(): ?int
+    {
+        return $this->accountNumber;
+    }
+
+    public function setAccountNumber(?int $accountNumber): self
+    {
+        $this->accountNumber = $accountNumber;
+
+        return $this;
+    }
+}
