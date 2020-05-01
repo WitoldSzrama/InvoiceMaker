068d168f74d8d1061f62289eb3575c9e42a374a8
Witold Szrama <witold.szrama@programa.pl>
2020-05-01 08:44:22 +0200

    Add translations

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -6,20 +6,47 @@ use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class CompanyType extends AbstractType
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('nip')
-            ->add('regon')
-            ->add('city')
-            ->add('postCode')
-            ->add('street')
-            ->add('stNumber')
-            ->add('accountNumber')
+            ->add('name', null, [
+                'label' => $this->translator->trans('company.name', [], 'labels')
+            ])
+            ->add('nip', null, [
+                'label' => $this->translator->trans('company.nip', [], 'labels')
+                ])
+            ->add('regon', null, [
+                'label' => $this->translator->trans('company.regon', [], 'labels')
+            ])
+            ->add('city', null, [
+                'label' => $this->translator->trans('company.city', [], 'labels')
+            ])
+            ->add('postCode', null, [
+                'label' => $this->translator->trans('company.postCode', [], 'labels')
+            ])
+            ->add('street', null, [
+                'label' => $this->translator->trans('company.street', [], 'labels')
+            ])
+            ->add('stNumber', null, [
+                'label' => $this->translator->trans('company.streetNumber', [], 'labels')
+            ])
+            ->add('accountNumber', null, [
+                'label' => $this->translator->trans('company.accountNumber', [], 'labels')
+            ])
         ;
     }
 
